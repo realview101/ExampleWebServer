a722570dfe85aab09433e915dc8ff0fa38feab49
realview101 <46009247+realview101@users.noreply.github.com>
2018-12-19 16:21:32 -0600

    Add files via upload

---
+++ ClientStream.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWebServer
+{
+    public interface IWebService
+    {
+        string processRequestCallBack(string request);
+    }
+
+    public class WebServer
+    {
+        private TcpListener server = null;
+
+        public WebServer(string ipAddress, int port)
+        {
+            IPAddress localAddr = IPAddress.Parse(ipAddress);
+            server = new TcpListener(localAddr, port);
+            server.Start();
+
+        }
+        public void Stop()
+        {
+            server.Stop();
+      }
+
+        async public Task<string >processRequest(IWebService callback)
+        {
+            TcpClient client = await server.AcceptTcpClientAsync();
+            NetworkStream stream = client.GetStream();
+            string response = callback.processRequestCallBack(GetRequest(stream));
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
+            stream.Write(msg, 0, msg.Length);
+            client.Close();
+            return response;
+
+        }
+        private  async Task<String>  GetRequestAsync(NetworkStream stream)
+        {
+
+ 
+            Byte[] bytes = new Byte[256];
+            String data = null;
+            StringBuilder totalString = new StringBuilder();
+            int i;
+
+            while (stream.DataAvailable && (i = await stream.ReadAsync(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                totalString.Append(data);
+            }
+            return totalString.ToString();
+        }
+        public  String GetRequest(NetworkStream stream)
+        {
+            return GetRequestAsync(stream).Result.ToString();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -9,51 +10,72 @@ using System.Threading.Tasks;
 
 namespace ExampleWebServer
 {
-    class Program
+  static   class Router
     {
-        static void Main(string[] args)
+        public static string pathToPage(string whichPage)
         {
-            TcpListener server = null;
-            try
+            string[] segments = System.Environment.CurrentDirectory.Split('\\');
+            StringBuilder rstr = new StringBuilder();
+            foreach (string seg in segments)
             {
-                string connetionString = "Data Source=54.213.195.209;Initial Catalog=Example;User ID=example;Password=example";
-                SqlConnection cnn = new SqlConnection(connetionString);
-                try
-                {
-                    cnn.Open();                    
-                    cnn.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Can not open data connection");
-                }
+                if (seg == "bin") break;
+                if (!seg.Contains(":")) rstr.Append("\\");
 
-                Int32 port = 8081;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-                server = new TcpListener(localAddr, port);
-                server.Start();
+                rstr.Append(seg);
+            }
+            rstr.Append("\\Pages\\");
+            rstr.Append( whichPage);
+            return rstr.ToString();
+        }
+        public static string ABC()
+        {
+            string page = pathToPage("ABC\\Index.html");
+            string text = System.IO.File.ReadAllText(page);
+            return text;
+        }
+        public static string DEF()
+        {
+            string page = pathToPage("DEF\\Index.html");
+            string text = System.IO.File.ReadAllText(page);
+            return text;
+        }
+        public static string Home()
+        {
+            string page = pathToPage("Home\\Index.html");
+            string text = System.IO.File.ReadAllText(page);
+            return text;
+        }
+        public static string Error()
+        {
+            string page = pathToPage("Error\\Index.html");
+            string text = System.IO.File.ReadAllText(page);
+            return text;
+        }
+    }
+    class WebServiceRequest : IWebService
+    {
+        public string processRequestCallBack(string request)
+        {
+            String body = "";
+            string[] urlArray = request.Split(' ');
+            if (urlArray.Length < 1 || urlArray[0] != "GET" || urlArray[1] == "/" || urlArray[1].ToUpper() == "/HOME")
+            {
+                body = Router.Home();
+            }
+            else if (urlArray[1].ToUpper() == "/ABC" ) {
+                body = Router.ABC();
+            }
+            else if (urlArray[1].ToUpper() == "/DEF")
+            {
+                body = Router.DEF();
+            }
+            else
+            {
+                body = Router.Error();
+            }
+            Console.WriteLine("Request was: {0}", request);
 
-                Byte[] bytes = new Byte[256];
-                String data = null;
-
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected");
-
-                    data = null;
-                    NetworkStream stream = client.GetStream();
-
-                    int i;
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Console.Write("{0}", data);
-                    }
-
-                    String body = @"<html><body>Hello world</body></html>";
-                    String response =
+            String response =
 @"HTTP/1.1 200 OK
 Server: Example
 Accept-Ranges: bytes
@@ -61,20 +83,42 @@ Content-Length: " + body.Length.ToString() + @"
 Content-Type: text/html
 
 " + body;
+            return response;
+        }
+    }
 
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(response);
-                    stream.Write(msg, 0, msg.Length);
-                    Console.WriteLine("Sent: {0}", response);
-                    client.Close();
-                }
+    class program {
+        static  void   Main(string[] args)
+        {
+            WebServer Server = null;
+            try
+            {
+                //string connetionString = "Data Source=54.213.195.209;Initial Catalog=Example;User ID=example;Password=example";
+                //SqlConnection cnn = new SqlConnection(connetionString);
+                //try
+                //{
+                //    cnn.Open();                    
+                //    cnn.Close();
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine("Can not open data connection");
+                //}
+                Server = new WebServer("127.0.0.1", 8081);
+ 
+                Byte[] bytes = new Byte[256];
+                Console.WriteLine("Entering wait loop...");
+                while (true)
+                {
+                    Server.processRequest(new WebServiceRequest());                 }
             }
             catch (SocketException e)
             {
                 Console.WriteLine("SocketException: {0}", e);
             }
             finally
-            {                 
-                server.Stop();
+            {
+                Server.Stop();
             }
 
             Console.WriteLine("\nHit enter to continue...");
